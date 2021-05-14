3cbb732f4fb39e4b784564802c8b19504bfce0b0
박계원 <62559049+Kyewon-Park@users.noreply.github.com>
2021-05-14 09:58:36 +0900

    first commit

---
+++ src/main/java/kye/won/WonApplication.java
@@ -0,0 +1,13 @@
+package kye.won;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kye/won/WonApplicationTests.java
@@ -0,0 +1,13 @@
+package kye.won;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
