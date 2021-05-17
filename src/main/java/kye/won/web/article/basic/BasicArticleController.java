c2cf39b2a9a722fba35c58e047ed08dc0d53ac2f
박계원 <62559049+Kyewon-Park@users.noreply.github.com>
2021-05-17 14:15:19 +0900

    addForm with redirect attr
    addForm template added with redirect /status attribute

--- src/main/java/kye/won/web/article/basic/BasicArticleController.java
+++ src/main/java/kye/won/web/article/basic/BasicArticleController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -16,6 +17,7 @@ import java.util.List;
 public class BasicArticleController {
     private final ArticleRepository articleRepository;
 
+    //글 목록
     @GetMapping
     public String articles(Model model){
         List<Article> articles = articleRepository.findAll();
@@ -23,6 +25,7 @@ public class BasicArticleController {
         return "basic/articles";
     }
 
+    //게시글
     @GetMapping("/{articleId}")
     public String article(@PathVariable Long articleId, Model model){
         Article found = articleRepository.findById(articleId);
@@ -30,19 +33,36 @@ public class BasicArticleController {
         return "basic/article";
     }
 
+    //게시글 추가 폼
+    @GetMapping("/add")
+    public String addForm(){
+        return "basic/addForm";
+    }
+    //게시글 추가 내용 등록
+    @PostMapping("/add")
+    public String add(@ModelAttribute Article article, RedirectAttributes redirectAttributes){
+        Article saved = articleRepository.save(article);
+        redirectAttributes.addAttribute("articleId", saved.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/articles/{articleId}";
+    }
+
+
+    //게시글 수정 폼
     @GetMapping("/{articleId}/edit")
-    public String articleEdit(@PathVariable Long articleId, Model model){
+    public String editForm(@PathVariable Long articleId, Model model){
         Article found = articleRepository.findById(articleId);
         model.addAttribute("article", found);
         return "basic/editForm";
     }
-
+    //게시글 수정 내용 등록
     @PostMapping("/{articleId}/edit")
     public String edit(@PathVariable Long articleId, @ModelAttribute Article article){
         articleRepository.updateArticle(articleId, article);
         return "redirect:/basic/articles/{articleId}";
     }
 
+    //테스트 위해 객체 임시 등록
     @PostConstruct
     public void init(){
         articleRepository.save(new Article("Coding good", "Lorem Ipsum is simply dummy text of the printing"));
